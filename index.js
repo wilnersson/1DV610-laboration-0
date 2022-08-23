f820ec1ee60187f2625b849548f283b28726d9ba
Henrik Wilnersson <hw222nq@student.lnu.se>
2022-08-23 09:44:53 +0200

    Add prompt-sync module

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello World!')
+/**
+ * The main starting point of the application.
+ *
+ * @author Henrik Wilnersson <hw222nq@student.lnu.se>
+ * @version 1.0.0
+ */
+
+import promptSync from 'prompt-sync'
+
+const prompt = promptSync()
+
+const result = prompt('What is your name? ')
+
+console.log(result)
